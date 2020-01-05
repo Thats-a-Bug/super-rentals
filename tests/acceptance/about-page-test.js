cf01c42efb8dc4da2c7a4710123529f397fcbfe2
JoaoGFarias <joao@thatsabug.com>
2020-01-05 13:49:53 +0100

    COlors change

--- tests/acceptance/about-page-test.js
+++ tests/acceptance/about-page-test.js
@@ -7,7 +7,7 @@ import {
   visit,
 } from '@ember/test-helpers'
 
-import { percySnapshot } from 'ember-percy';
+import { percySnapshot } from 'ember-percy'
 
 module('Acceptance | about page', function(hooks) {
   setupApplicationTest(hooks);
@@ -17,12 +17,13 @@ module('Acceptance | about page', function(hooks) {
     await visit('/rentals');
     await click(".button-to-about");
     assert.equal(currentURL(), '/about', 'should navigate to about');
-    await percySnapshot("button to home on about page", {scope: ".body"});
+    await percySnapshot("button to home about page", {scope: ".body"})
   });
   test('should link to home page', async function(assert) {
     await visit('/about');
     await click(".button-to-home");
     assert.equal(currentURL(), '/rentals', 'should navigate to home');
+    await percySnapshot("button home page from about", {scope: ".body"})
   });
 
 });
